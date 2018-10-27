9fc2cc0bf4000a55ce25ae8a4436b56d34c2a0a5
Arne Deloose <Arne.Deloose@UGent.be>
2018-10-27 15:44:19 +0200

    Comparison_other

---
+++ Code/Comparison_other.py
@@ -0,0 +1,18 @@
+#Load data
+import os
+path='C:/Users/arne/Documents/Github/Masterproef2018Code/Code';
+os.chdir(path)
+import AD_functions as AD
+path='C:/Users/arne/Documents/School/Thesis'; #Change this to directory that stores the data
+os.chdir(path)
+
+file_name1='.wav'
+file_name2='.wav'
+file_name3='.wav'
+
+rectangles1, regions1, spectros1=AD.spect_loop(file_name1)
+rectangles2, regions2, spectros2=AD.spect_loop(file_name2)
+rectangles3, regions3, spectros3=AD.spect_loop(file_name3)
+
+
+AD.show_mregions(rectangles1, spectros1)
