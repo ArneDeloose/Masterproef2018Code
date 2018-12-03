ec550c93b4bc6189f7d700c11105c29f3a481fa3
Arne Deloose <Arne.Deloose@UGent.be>
2018-12-03 12:56:28 +0100

    Implement SOM

--- Code/AD_functions.py
+++ Code/AD_functions.py
@@ -1,3 +1,4 @@
+from __future__ import division #changes / to 'true division'
 import scipy.io.wavfile
 import numpy as np
 import math
@@ -882,4 +883,128 @@ def loading_init(**optional): #loads in certain things so they only run once
     num_bats, num_total=AD.set_numbats(list_bats, **optional)
     freq_bats, freq_range_bats, freq_peakT_bats, freq_peakF_bats=AD.set_batfreq(rectangles_temp, regions_temp, list_bats, num_bats)
     return(freq_bats, freq_range_bats, freq_peakT_bats, freq_peakF_bats, list_bats, colors_bat, num_bats, num_total, regions_temp, rectangles_temp)
+
+def SOM(raw_data, network_dim, n_iter, init_learning_rate, normalise_data, normalise_by_column):
+    m = raw_data.shape[0]
+    n = raw_data.shape[1]
+    # initial neighbourhood radius
+    init_radius = max(network_dim[0], network_dim[1]) / 2
+    # radius decay parameter
+    time_constant = n_iter / np.log(init_radius)
+    if normalise_data:
+        if normalise_by_column:
+            # normalise along each column
+            col_maxes = raw_data.max(axis=0)
+            data = raw_data / col_maxes[np.newaxis, :]
+        else:
+            # normalise entire dataset
+            data = raw_data / data.max()
+    else:
+        data=raw_data
+    # setup random weights between 0 and 1
+    # weight matrix needs to be one m-dimensional vector for each neuron in the SOM
+    net = np.random.random((network_dim[0], network_dim[1], m))
+    for i in range(n_iter):
+        # select a training example at random
+        t = data[:, np.random.randint(0, n)].reshape(np.array([m, 1]))    
+        # find its Best Matching Unit
+        bmu, bmu_idx = AD.find_bmu(t, net, m)
+        # decay the SOM parameters
+        r = AD.decay_radius(init_radius, i, time_constant)
+        l = AD.decay_learning_rate(init_learning_rate, i, n_iter)
+        # now we know the BMU, update its weight vector to move closer to input
+        # and move its neighbours in 2-D space closer
+        # by a factor proportional to their 2-D distance from the BMU
+        for x in range(net.shape[0]):
+            for y in range(net.shape[1]):
+                w = net[x, y, :].reshape(m, 1)
+                # get the 2-D distance (again, not the actual Euclidean distance)
+                w_dist = np.sum((np.array([x, y]) - bmu_idx) ** 2)
+                # if the distance is within the current neighbourhood radius
+                if w_dist <= r**2:
+                    # calculate the degree of influence (based on the 2-D distance)
+                    influence = AD.calculate_influence(w_dist, r)
+                    # now update the neuron's weight using the formula:
+                    # new w = old w + (learning rate * influence * delta)
+                    # where delta = input vector (t) - old w
+                    new_w = w + (l * influence * (t - w))
+                    # commit the new weight
+                    net[x, y, :] = new_w.reshape(1, m)
+    return(net)
+
+def find_bmu(t, net, m):
+    """
+        Find the best matching unit for a given vector, t, in the SOM
+        Returns: a (bmu, bmu_idx) tuple where bmu is the high-dimensional BMU
+                 and bmu_idx is the index of this vector in the SOM
+    """
+    bmu_idx = np.array([0, 0])
+    # set the initial minimum distance to a huge number
+    min_dist = np.iinfo(np.int).max    
+    # calculate the high-dimensional distance between each neuron and the input
+    for x in range(net.shape[0]):
+        for y in range(net.shape[1]):
+            w = net[x, y, :].reshape(m, 1)
+            # don't bother with actual Euclidean distance, to avoid expensive sqrt operation
+            sq_dist = np.sum((w - t) ** 2)
+            if sq_dist < min_dist:
+                min_dist = sq_dist
+                bmu_idx = np.array([x, y])
+    # get vector corresponding to bmu_idx
+    bmu = net[bmu_idx[0], bmu_idx[1], :].reshape(m, 1)
+    # return the (bmu, bmu_idx) tuple
+    return (bmu, bmu_idx)
+
+def decay_radius(initial_radius, i, time_constant):
+    return (initial_radius * np.exp(-i / time_constant))
+
+def decay_learning_rate(initial_learning_rate, i, n_iter):
+    return (initial_learning_rate * np.exp(-i / n_iter))
+
+def calculate_influence(distance, radius):
+    return (np.exp(-distance / (2* (radius**2))))
+
+def calc_Umat(net):
+    m = net.shape[0]
+    n = net.shape[1]
+    U=np.zeros((m, n), dtype=np.float)
+    for i in range(m):
+        for j in range(n):
+            if i==0:
+                if j==0:
+                    U[i,j]=np.linalg.norm(net[i,j,:]-net[i,j+1,:])+np.linalg.norm(net[i,j,:]-net[i+1,j,:])
+                elif j==n-1:
+                    U[i,j]=np.linalg.norm(net[i,j,:]-net[i,j-1,:])+np.linalg.norm(net[i,j,:]-net[i+1,j,:])
+                else:
+                    U[i,j]=np.linalg.norm(net[i,j,:]-net[i,j+1,:])+np.linalg.norm(net[i,j,:]-net[i+1,j,:])+ \
+                    np.linalg.norm(net[i,j,:]-net[i,j-1,:])
+            elif i==m-1:
+                if j==0:
+                    U[i,j]=np.linalg.norm(net[i,j,:]-net[i,j+1,:])+np.linalg.norm(net[i,j,:]-net[i-1,j,:])
+                elif j==n-1:
+                    U[i,j]=np.linalg.norm(net[i,j,:]-net[i,j-1,:])+np.linalg.norm(net[i,j,:]-net[i-1,j,:])
+                else:
+                    U[i,j]=np.linalg.norm(net[i,j,:]-net[i,j+1,:])+np.linalg.norm(net[i,j,:]-net[i-1,j,:])+ \
+                    np.linalg.norm(net[i,j,:]-net[i,j-1,:])
+            elif j==0:
+                U[i,j]=np.linalg.norm(net[i,j,:]-net[i,j+1,:])+np.linalg.norm(net[i,j,:]-net[i+1,j,:])+ \
+                    np.linalg.norm(net[i,j,:]-net[i-1,j,:])
+            elif j==n-1:
+                U[i,j]=np.linalg.norm(net[i,j,:]-net[i,j-1,:])+np.linalg.norm(net[i,j,:]-net[i-1,j,:])+ \
+                    np.linalg.norm(net[i,j,:]-net[i+1,j,:])
+            else:
+                U[i,j]=np.linalg.norm(net[i,j,:]-net[i,j+1,:])+np.linalg.norm(net[i,j,:]-net[i,j-1,:])+ \
+                np.linalg.norm(net[i,j,:]-net[i+1,j,:])+np.linalg.norm(net[i,j,:]-net[i-1,j,:])
+    return(U)
+
+def calc_BMU_scores(data, net):
+    m=data.shape[0]
+    n=data.shape[1] #number of data points
+    score_BMU=np.zeros((n, 2), dtype=np.uint8)
+    for i in range(n):
+        t = data[:, i].reshape(np.array([m, 1]))    
+        _, bmu_idx=AD.find_bmu(t, net, m)
+        score_BMU[i, 0]=bmu_idx[0]
+        score_BMU[i, 1]=bmu_idx[1]
+    return(score_BMU)
     
