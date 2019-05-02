98fd4b0c9361b55cf3a3602edf45427f9934d055
Arne Deloose <Arne.Deloose@UGent.be>
2019-05-02 15:36:16 +0200

    Fix bug

--- Code (not used)/write_eval_templates(Barataud).py
+++ Code (not used)/write_eval_templates(Barataud).py
@@ -13,7 +13,7 @@ os.chdir(path)
 freq_bats, freq_range_bats, freq_peakT_bats, freq_peakF_bats, list_bats, colors_bat, num_bats, num_total, regions_temp, rectangles_temp=AD1.loading_init()
 
 #test files
-name_file=''
+name_file='080_FM-nasal_B-barbastellus_appr_FME-H1_Hte-Vienne_June2010_M-Barataud'
 
 rectangles, regions, spectros=AD2.spect_loop('Barataud_CD_TimeExpansion10x/'+name_file + '.wav', exp_factor=10)
 
--- Data/Modules/AD2_Spectro.py
+++ Data/Modules/AD2_Spectro.py
@@ -151,7 +151,7 @@ def ROI(spect_norm, kern, X):
     #dilation
     kernel = np.ones((kern,kern), np.uint8)
     img_dilation = cv2.dilate(thresh, kernel, iterations=1)
-    _,ctrs, _ = cv2.findContours(img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+    ctrs, _ = cv2.findContours(img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
     #Retr_external: retrieval mode external. Only outer edges are considered,
     #Contours within other contours aren't allowed, any holes are ignored
     #Chain_approx_simple: stores only the outer points of the rectangle (4 points)   
